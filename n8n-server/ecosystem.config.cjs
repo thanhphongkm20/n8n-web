cc5b42c8ff1512f9e1a02a24075994c09adabc0f
Lê Đình Thanh Phong <167110799+thanhphongkm20@users.noreply.github.com>
2026-04-20 14:58:46 +0700

    Update ecosystem.config.cjs

--- n8n-server/ecosystem.config.cjs
+++ n8n-server/ecosystem.config.cjs
@@ -1,15 +1,10 @@
 module.exports = {
-  apps: [
-    {
-      name: "n8n-server",
-      script: "./index.js",
-      watch: false,
-      log_date_format: "YYYY-MM-DD HH:mm:ss",
-      env: {
-        NODE_ENV: "production",
-        TZ: "Asia/Bangkok",
-        PORT: 5000
-      },
-    },
-  ],
-};
+  apps: [{
+    name: "n8n-server",
+    script: "index.js",
+    env: {
+      NODE_ENV: "production",
+      PORT: 5000
+    }
+  }]
+}
